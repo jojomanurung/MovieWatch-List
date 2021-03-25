59ee7cf2ce697fb1e6b4f95f07dd5ef831c17b78
Joshua Melvin <joshuamlvn@gmail.com>
2021-03-25 20:10:54 +0700

    updating for edit and delete method

--- src/app/dialog.service.ts
+++ src/app/dialog.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Movie, movies } from './movie-data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DialogService {
+  moviesList: BehaviorSubject<Movie[]>;
+  data: Array<Movie> = [];
 
-  constructor() { }
+  constructor() {
+    this.moviesList = new BehaviorSubject([]);
+    this.data = movies;
+  }
+
+  getAll() {
+    this.moviesList.next(this.data);
+  }
+
+  add(moviess: Movie) {
+    this.data.push(moviess);
+  }
+
+  edit(moviess: Movie) {
+    let findElem = this.data.find(m => m.id == moviess.id);
+    findElem.title = moviess.title;
+    findElem.episodes = moviess.episodes;
+    findElem.info_url = moviess.info_url;
+    findElem.watch_url = moviess.watch_url;
+    this.moviesList.next(this.data);
+  }
+
+  remove(id: string) {
+    this.data = this.data.filter(m => {
+      return m.id != id
+    });
+
+    this.moviesList.next(this.data);
+  }
+
 }
