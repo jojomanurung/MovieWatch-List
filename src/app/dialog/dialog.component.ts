be53ed3738cef83c0bcc2cc9999a6b7864d01d95
Joshua Melvin <joshuamlvn@gmail.com>
2021-03-25 20:14:48 +0700

    update FormControl

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Movie, movies } from '../movie-data';
+
 
 @Component({
   selector: 'app-dialog',
@@ -8,14 +10,29 @@ import { Movie, movies } from '../movie-data';
   styleUrls: ['./dialog.component.css']
 })
 export class DialogComponent implements OnInit {
-  data: Movie[] = movies;
+
+  formInstance: FormGroup;
 
   constructor(
     private dialogRef: MatDialogRef<DialogComponent>,
     @Inject(MAT_DIALOG_DATA) public dat: Movie
-  ) {}
+  ) { this.formInstance = new FormGroup({
+    "id": new FormControl('',),
+    "title": new FormControl('', Validators.required),
+    "episodes": new FormControl(''),
+    "info_url": new FormControl(''),
+    "watch_url": new FormControl('')
+  });
+  this.formInstance.setValue(dat);
+  }
+
 
   ngOnInit(): void {
+
+  }
+
+  save(): void {
+    this.dialogRef.close(Object.assign(new Movie(), this.formInstance.value));
   }
 
   close() {
