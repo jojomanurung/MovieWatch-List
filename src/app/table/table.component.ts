60cc2d1484df84d4da8ca019e31bb5baad341c61
Joshua Melvin <joshuamlvn@gmail.com>
2021-03-29 08:19:23 +0700

    change method name and fix add method

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnDestroy, OnInit, } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
-import { Subscription } from 'rxjs';
 
-import { Movie } from '../movie-data';
 import { DialogComponent } from '../dialog/dialog.component';
 import { DialogService } from '../dialog.service';
 import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
+import { Movie } from '../movie-data';
 
 @Component({
   selector: 'app-table',
@@ -15,59 +15,59 @@ import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.compone
 })
 export class TableComponent implements OnInit, OnDestroy {
   public displayedColumns: string[] = [ 'title', 'episodes', 'info_url', 'watch_url', 'action'];
-
   public dataSource: MatTableDataSource<Movie>;
   private serviceSubscribe: Subscription;
 
   constructor(
     public dialog: MatDialog,
-    private dialogService: DialogService
-  ) { this.dataSource = new MatTableDataSource<Movie>();
+    private dialogService: DialogService,
+  ) {
+    this.dataSource = new MatTableDataSource<Movie>();
   }
 
   ngOnInit(): void {
     this.dialogService.getAll();
-    this.serviceSubscribe = this.dialogService.moviesList.subscribe(res => {
+    this.serviceSubscribe = this.dialogService._data.subscribe(res => {
       this.dataSource.data = res;
-    });
+    })
   }
+
 
   openLink(url) {
     window.open(url, '_blank');
   }
 
-  add(data: Movie) {
+  addMovie() {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: '600px'
+    });
+
+    dialogRef.afterClosed().subscribe(add => {
+      if(add) {
+        this.dialogService.addMovie(add);
+      }
+    });
+  }
+
+  editMovie(data: Movie) {
     const dialogRef = this.dialog.open(DialogComponent, {
       width: '600px',
       data: data
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      if(result) {
-        this.dialogService.add(result);
+    dialogRef.afterClosed().subscribe(editData => {
+      if(editData) {
+        this.dialogService.editMovie(editData);
       }
     });
   }
 
-  edit(data: Movie) {
-    const dialogRef = this.dialog.open(DialogComponent, {
-      width: '600px',
-      data: data
-    });
+  deleteMovie() {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {});
 
-    dialogRef.afterClosed().subscribe(result => {
-      if(result) {
-        this.dialogService.edit(result);
-      }
-    });
-  }
-
-  delete(id: any) {
-    const dialogRef = this.dialog.open(ConfirmDialogComponent);
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        this.dialogService.remove(id);
+    dialogRef.afterClosed().subscribe(deleteData => {
+      if(deleteData) {
+        this.dialogService.removeMovie(deleteData);
       }
     });
   }
